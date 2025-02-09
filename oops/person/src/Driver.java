bcce4d0f13d91d29aa14c50c410f723fd83ebaa7
Saikumar Adepu <saikumaraadepu1@gmail.com>
2025-02-09 09:24:08 +0530

    Added getPerson method variants to person module

--- oops/person/src/Driver.java
+++ oops/person/src/Driver.java
@@ -6,6 +6,12 @@ public class Driver {
 
         Person p = d.getPerson();
         System.out.println(p.id + " " + p.name + " " + p.age);
+
+        Person[] persons = d.getPersonsArray();
+
+        d.printPerson2(persons);
+
+        d.printPerson1(persons[0], persons[1]);
     }
 
     Person getPerson() {
@@ -16,5 +22,35 @@ public class Driver {
         p.age = 22;
 
         return p;
+    }
+
+    void printPerson1(Person p1, Person p2) {
+        System.out.println(p1.id + " " + p1.name + " " + p1.age);
+        System.out.println(p2.id + " " + p2.name + " " + p2.age);
+    }
+
+    void printPerson2(Person[] p) {
+
+        for (Person person : p) {
+            System.out.println(person.id + " " + person.name + " " + person.age);
+        }
+    }
+
+    Person[] getPersonsArray() {
+
+        Person p1 = new Person();
+        p1.id = 101;
+        p1.name = "sai";
+        p1.age = 22;
+        Person p2 = new Person();
+        p2.id = 102;
+        p2.name = "kumar";
+        p2.age = 23;
+        Person p3 = new Person();
+        p3.id = 103;
+        p3.name = "adepu";
+        p3.age = 22;
+
+        return new Person[] {p1, p2, p3};
     }
 }
